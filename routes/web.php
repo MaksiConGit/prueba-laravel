9a4d4e018d5e2da219a3e1da32c84fdf514932e8
MaksiConGit <122688647+MaksiConGit@users.noreply.github.com>
2024-08-26 15:36:20 -0300

    feat: se crearon rutas, controladores y vista

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,22 @@
 <?php
 
+use App\Http\Controllers\AlumnosController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\InicioController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::get('/', HomeController::class);
+Route::get('/inicio', InicioController::class);
+Route::get('/alumnos', [AlumnosController::class, 'index']);
+Route::get('/alumnos/añadir', [AlumnosController::class, 'añadir']);
+Route::get('/alumnos/{alumno}', [AlumnosController::class, 'mostrarAlumno']);
+
+//Get
+//Post
+//Put
+//Patch
+//Delete
